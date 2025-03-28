2b72093416e0f3fc28d30e768e3b1baa49c6e795
Kayki Letieri <kaykiletieri37@gmail.com>
2025-03-28 01:45:41 -0300

    feat: implement login page with input and button components

---
+++ src/app/components/primary-button/primary-button.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  imports: [],
+  templateUrl: './primary-button.component.html',
+  styleUrl: './primary-button.component.scss'
+})
+export class PrimaryButtonComponent {
+  @Input() disabled: boolean = false;
+  @Output() onClick = new EventEmitter<Event>();
+
+  handleClick(event: Event) {
+    if (!this.disabled) {
+      this.onClick.emit(event);
+    }
+  }
+}
