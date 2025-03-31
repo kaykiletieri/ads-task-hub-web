7f3ea4ac4afc0dfdfe4449e8574ba6e525c4844d
Kayki Letieri <kaykiletieri37@gmail.com>
2025-03-31 00:42:25 -0300

    feat: add NotFound component and route for 404 error handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
+    { path: '**', component: NotFoundComponent },
 ];
