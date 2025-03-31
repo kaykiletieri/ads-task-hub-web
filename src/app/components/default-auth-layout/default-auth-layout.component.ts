fb7cbf30867e9d2684d09811471f2f68bf43759b
Kayki Letieri <kaykiletieri37@gmail.com>
2025-03-30 23:45:31 -0300

    feat: add navigation links to login and registration pages

--- src/app/components/default-auth-layout/default-auth-layout.component.ts
+++ src/app/components/default-auth-layout/default-auth-layout.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-default-auth-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './default-auth-layout.component.html',
   styleUrl: './default-auth-layout.component.scss'
 })
 export class DefaultAuthLayoutComponent {
   @Input() title: string = '';
   @Input() subtitle: string = '';
+  @Input() primaryLinkText: string = '';
+  @Input() secondLinkText: string = '';
+  @Input() linkUrl: string = '';
+  showLink: boolean = true;
 }
